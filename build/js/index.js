42422982f66028371923ba1877674a177fc07ae9
Yang Vanya <vaniafed@WebMachine.WebServer>
2018-04-17 21:59:33 +0300

    maybe finished

--- build/js/index.js
+++ build/js/index.js
@@ -14,6 +14,15 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 window.onload = function () {
     
+    var linkToTop = document.querySelector('.link-to-top');
+    
+    window.addEventListener('scroll', function() {
+        if (window.pageYOffset > 500) {
+            linkToTop.style.opacity = 1;
+        } else {
+            linkToTop.style.opacity = 0;
+        }
+    });
 }
 
 
--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -4,5 +4,14 @@ import "./index.scss";
 
 window.onload = function () {
     
+    var linkToTop = document.querySelector('.link-to-top');
+    
+    window.addEventListener('scroll', function() {
+        if (window.pageYOffset > 500) {
+            linkToTop.style.opacity = 1;
+        } else {
+            linkToTop.style.opacity = 0;
+        }
+    });
 }
 
